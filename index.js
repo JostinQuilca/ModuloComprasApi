12097f5c3a748410edc8b402cd56da05832525f8
JostinQuilca <jdquilcap1@utn.edu.ec>
2025-06-10 21:16:35 -0500

    Ajustes para Vercel: api/index.js y limpieza de vercel.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const serverless = require('serverless-http');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../src/docs/swaggerConfig');
-
 const app = require('../src/app');
 
-// (1) Asegura que la ruta esté activa desde aquí también
+// Cargar la documentación Swagger desde aquí
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Exportar como handler para Vercel
 module.exports = serverless(app);
